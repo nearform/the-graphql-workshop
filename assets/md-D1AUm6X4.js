bed7d82cfbc00bf1141ced01952776fe5585d34a
mahdilamb <mahdilamb@users.noreply.github.com>
2025-12-10 09:16:18 +0000

    deploy: 4b9f56ff87c8c6390194750a6472edac69e83161

---
+++ assets/md-D1AUm6X4.js
@@ -0,0 +1,23 @@
+import{_ as h}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-SS6pBPJt.js";import{b as r,o as p,w as e,g as s,e as d,m as o,ad as i,v as c,x as g,T as l}from"./modules/vue-CmoBDYQ5.js";import{I as y}from"./slidev/default-BD0M2nTj.js";import{u as D,f as m}from"./slidev/context-CyHergH_.js";import"./modules/unplugin-icons-CktKC814.js";import"./index-x3mZFWWs.js";import"./modules/shiki-BXd5L9bY.js";const B={class:"middle-flex"},N={__name:"slides.md__slidev_45",setup(A){const{$clicksContext:n,$frontmatter:t}=D();return n.setup(),(C,a)=>{const k=h;return p(),r(y,c(g(l(m)(l(t),44))),{default:e(()=>[a[1]||(a[1]=s("h1",null,"Step 11: Solution / 1",-1)),s("div",B,[d(k,o({},{title:"",ranges:["1,4-11,14"]}),{default:e(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-js"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// services/service1.js")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"const"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," service1"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ="),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#B8A965","--shiki-light":"#998418"}},"  schema"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},":"),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}}," `")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  enum Role {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    ADMIN")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    VERIFIED")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  }")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  directive @auth(")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    role: Role")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  ) on OBJECT | FIELD_DEFINITION")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  extend type Query {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    me: User @auth(role: VERIFIED)")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  }")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  ")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},'  type User @key(fields: "id") {')]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    id: ID!")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"    name: String!")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"  }")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},"  `"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},",")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"  ...")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"}")])])],-1)])]),_:1},16)])]),_:1},16)}}};export{N as default};
